50920540634c9e0d35bd745b45e17a6f3b08dafc
christianinho1-a11y <christianinho1@gmail.com>
2026-01-30 23:35:16 -0500

    Delete partials directory

--- partials/footer.php
+++
@@ -1,8 +0,0 @@
-    </main>
-    <footer class="site-footer">
-        <p>Tech Lab Arcade &copy; <?php echo date('Y'); ?>. Built for classroom learning.</p>
-    </footer>
-    <script src="assets/js/theme-toggle.js"></script>
-    <script src="assets/js/main.js"></script>
-</body>
-</html>
